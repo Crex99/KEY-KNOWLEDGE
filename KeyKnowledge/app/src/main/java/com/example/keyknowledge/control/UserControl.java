d5caaa0875f89b5251fed2253e48b8e1c0eb1064
crex99 <crexenxo@gmail.com>
2021-01-26 23:34:47 +0100

    UserManager added

--- KeyKnowledge/app/src/main/java/com/example/keyknowledge/control/UserControl.java
+++ KeyKnowledge/app/src/main/java/com/example/keyknowledge/control/UserControl.java
@@ -15,48 +15,29 @@ import com.google.firebase.database.ValueEventListener;
 
 public class UserControl {
 
-    private static final String TABLE="users",OFFLINE="offline",ONLINE="online";
-
-    private DatabaseReference mDatabase;
+    private UserManager userManager;
     private MainActivity activity;
 
-    private boolean result=false;
-
     public UserControl(){
-        mDatabase = FirebaseDatabase.getInstance().getReference();
+        userManager =new UserManager();
     }
 
     public UserControl(MainActivity a){
         activity=a;
-        mDatabase = FirebaseDatabase.getInstance().getReference();
+        userManager =new UserManager();
     }
 
-    public void addUser(User a){
-        mDatabase.child(TABLE).child(a.getNickname()).setValue(a);
+    public void addUser(User user){
+        userManager.addUser(user);
     }
+
+    public void setMessage(String x){
+        activity.message(x);
+    }
+
+
 
     public void setUserOnline(String nick,String pass){
-        mDatabase.child(TABLE).addListenerForSingleValueEvent(new ValueEventListener(){
-
-
-            @Override
-            public void onDataChange( DataSnapshot snapshot) {
-
-                String current=snapshot.child(nick).child("password").getValue(String.class);
-
-                if(current.equals(pass)){
-                    mDatabase.child(TABLE).child(nick).child("state").setValue(ONLINE);
-                    activity.message("utente aggiornato");
-                }else{
-                    activity.message("utente non aggiornato");
-                }
-                }
-
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
+        userManager.setUserOnline(nick,pass,this);
     }
 }
