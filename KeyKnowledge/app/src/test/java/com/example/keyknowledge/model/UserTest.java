6812592576f694a13e31a635690b0ae109d415ca
nico0099 <72070319+nico0099@users.noreply.github.com>
2021-03-09 11:27:32 +0100

    testing 1.0

---
+++ KeyKnowledge/app/src/test/java/com/example/keyknowledge/model/UserTest.java
@@ -0,0 +1,13 @@
+package com.example.keyknowledge.model;
+
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class UserTest extends TestCase {
+
+    private User user;
+
+    @Override
+    protected void setUp() throws Exception {
+    }
+}
